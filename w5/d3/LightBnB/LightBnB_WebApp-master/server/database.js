b0b3b918f81f326ec58b365bbad41ded53e44abb
Gulbike Biyekenova <gbknva@gmail.com>
2021-11-26 00:55:25 +0000

    updates function addProperty to to save a new property to properties table

--- w5/d3/LightBnB/LightBnB_WebApp-master/server/database.js
+++ w5/d3/LightBnB/LightBnB_WebApp-master/server/database.js
@@ -34,7 +34,6 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  // return Promise.resolve(users[id]);
   return pool
   .query(`SELECT * FROM users WHERE id=$1`,[id])
   .then((result) => {
@@ -90,25 +89,16 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-//  const getAllProperties = (options, limit = 10) => {
-//   return pool
-//     .query(`SELECT * FROM properties LIMIT $1`, [limit])
-//     .then((result) => result.rows)
-//     .catch((err) => {
-//       console.log(err.message);
-//     });
-// };
 const getAllProperties = function (options, limit = 10) {
-  // 1
+
   const queryParams = [];
-  // 2
+
   let queryString = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
   JOIN property_reviews ON properties.id = property_id
   `;
 
-  // 3
   if (options.city) {
     if (queryParams.length > 0) {
 
@@ -147,7 +137,6 @@ const getAllProperties = function (options, limit = 10) {
       }
   }
 
-  // 4
   queryParams.push(limit);
   queryString += `
   GROUP BY properties.id
@@ -155,10 +144,8 @@ const getAllProperties = function (options, limit = 10) {
   LIMIT $${queryParams.length};
   `;
 
-  // 5
   console.log(queryString, queryParams);
 
-  // 6
   return pool.query(queryString, queryParams).then((res) => res.rows);
 };
  exports.getAllProperties = getAllProperties;
@@ -168,10 +155,27 @@ const getAllProperties = function (options, limit = 10) {
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
-}
+const addProperty = function (property) {
+  return pool
+  .query(`INSERT INTO properties (
+    title,
+    description,
+    thumbnail_photo_url,
+    cover_photo_url,
+    cost_per_night,
+    street,
+    city,
+    province,
+    post_code,
+    country,
+    parking_spaces,
+    number_of_bathrooms,
+    number_of_bedrooms) 
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *`, [property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
+  .then((result) => {
+    return result.rows[0]}
+    )
+  .catch((err) => console.log(err))
+
+};
 exports.addProperty = addProperty;
